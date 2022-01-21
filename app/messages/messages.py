ec5e2f5b74be409c3596a190553f5b1cdde9513d
Sebastian Pietras <01133337@pw.edu.pl>
2022-01-21 23:23:36 +0100

    Cleaned up messages

--- app/messages/messages.py
+++ app/messages/messages.py
@@ -1,28 +1,44 @@
+from abc import abstractmethod
 from datetime import datetime
 from enum import Enum
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from pydantic import BaseModel
 from spade.message import Message
 
 ONTOLOGY = "aasd_drones_boarder"
-LANGUAGE = "aasd_boarder_lang"
+LANGUAGE = "JSON"
 
 
 class MessageBase(BaseModel):
     def make_message(self, to: str, sender: str) -> Message:
-        message = Message(to=to, sender=sender)
-        self.set_default_metadata(message)
-        message.body = self.json()
-        return message
+        return Message(
+            to=to,
+            sender=sender,
+            body=self.json(),
+            metadata=self.metadata
+        )
 
     def make_response(self, message: Message) -> Message:
-        return self.make_message(to=str(message.sender), sender=str(message.to))
-    
-    def set_default_metadata(self, msg: Message) -> None:
-        msg.set_metadata("ontology", ONTOLOGY)
-        msg.set_metadata("language", LANGUAGE)
-        msg.set_metadata("body_type", self.__class__.__name__)
+        return self.make_message(
+            to=str(message.sender),
+            sender=str(message.to)
+        )
+
+    @property
+    @abstractmethod
+    def performative(self) -> str:
+        pass
+
+    @property
+    def metadata(self) -> Dict[str, str]:
+        return {
+            "ontology"    : ONTOLOGY,
+            "language"    : LANGUAGE,
+            "performative": self.performative,
+            "body_type"   : self.__class__.__name__
+        }
+
 
 class Coordinates(BaseModel):
     x: float
@@ -48,125 +64,125 @@ class Dock(BaseModel):
 # Pierwszy diagram kolaboracji
 
 
-class HelpRequestBody(MessageBase):  # Prośba o pomoc
+class HelpRequestBody(MessageBase):
+    """Prośba o pomoc"""
+
     time: datetime
     position: Coordinates
     urgency: UrgencyEnum
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "request")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "request"
 
 
-class HelpOfferBody(MessageBase):  # Propozycja pomocy
+class HelpOfferBody(MessageBase):
+    """Propozycja pomocy"""
+
     time: datetime
     position: Coordinates
     eta: float
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "agree")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "agree"
 
 
-class HelpResponseBody(MessageBase):  # Zaakceptowanie lub odrzucenie pomocy
+class HelpResponseBody(MessageBase):
+    """Zaakceptowanie lub odrzucenie pomocy"""
+
     help_accepted: bool
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "inform")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "inform"
 
 
 # Drugi diagram kolaboracji
 
 
-class SectorClearedReportBody(MessageBase):  # Powiadomienie o przegonieniu zwierząt
+class SectorClearedReportBody(MessageBase):
+    """Powiadomienie o przegonieniu zwierząt"""
+
     count: int
     position: Coordinates
     seconds_spent: float
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "inform")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "inform"
 
 
-class SectorClearedRecievedBody(MessageBase):  # Powiadomienie o odebraniu raportu
+class SectorClearedRecievedBody(MessageBase):
+    """Powiadomienie o odebraniu raportu"""
+
     accepted: bool
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "agree")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "agree"
 
 
 # Trzeci diagram kolaboracji
 
 
-class SearchingStatusBody(MessageBase):  # Powiadomienie o stanie przeszukiwania
+class SearchingStatusBody(MessageBase):
+    """Powiadomienie o stanie przeszukiwania"""
+
     actual_position: Coordinates
     searching_range_meters: float
     boars_positions: List[Coordinates]
     heading_towards: Coordinates
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "inform")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "inform"
 
 
-class SearchingDirectivesBody(MessageBase):  # Powiadomienie o odebraniu raportu
+class SearchingDirectivesBody(MessageBase):
+    """Powiadomienie o odebraniu raportu"""
+
     keep_schedule: bool
     change_direction: Optional[Coordinates]
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "inform")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "inform"
 
 
 # Czwarty i piąty diagram kolaboracji
 
 
-class DockOccupationReportBody(MessageBase):  # Powiadomienie o zmianie zajętości stacji dokującej
+class DockOccupationReportBody(MessageBase):
+    """Powiadomienie o zmianie zajętości stacji dokującej"""
+
     total_docks: int
     status: List[Dock]
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "inform")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "inform"
 
 
 # Szósty diagram kolaboracji
 
-class ChargingRequestBody(MessageBase):  # Powiadomienie o chęci ładowania
+class ChargingRequestBody(MessageBase):
+    """Powiadomienie o chęci ładowania"""
+
     remaining_time_on_battery: float
     distance_in_seconds: float
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "request")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "request"
 
 
-class ChargingResponseBody(MessageBase):  # Powiadomienie o dostępnym miejscu do ładowania
+class ChargingResponseBody(MessageBase):
+    """Powiadomienie o dostępnym miejscu do ładowania"""
+
     charging_available: bool
     allocated_dock: Optional[Dock]
 
-    def make_message(self, to: str, sender: str) -> Message:
-        message = super().make_message(to, sender)
-        message.set_metadata("performative", "agree")
-
-        return message
+    @property
+    def performative(self) -> str:
+        return "agree"
